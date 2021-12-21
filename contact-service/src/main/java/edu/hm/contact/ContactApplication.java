f1530bcab949e4d044f4e7dce4b492ffdf75eb21
SimonHirner <simon.hirner@hm.edu>
2021-12-21 21:36:59 +0100

    Rework initializer.

--- contact-service/src/main/java/edu/hm/contact/ContactApplication.java
+++ contact-service/src/main/java/edu/hm/contact/ContactApplication.java
@@ -2,29 +2,65 @@ package edu.hm.contact;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Contact API", version = "1.0", description = "Contact Information"))
 public class ContactApplication implements CommandLineRunner {
 
-	private final ContactRepository contactRepository;
+    Logger logger = LoggerFactory.getLogger(ContactApplication.class);
 
-	public ContactApplication(ContactRepository contactRepository) {
-		this.contactRepository = contactRepository;
-	}
+    private final ContactRepository contactRepository;
+    private final AddressRepository addressRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContactApplication.class, args);
-	}
+    public ContactApplication(ContactRepository contactRepository, AddressRepository addressRepository) {
+        this.contactRepository = contactRepository;
+        this.addressRepository = addressRepository;
+    }
 
-	@Override
-	public void run(String... args) {
-		// contactRepository.deleteAll();
-		// contactRepository.save(new ContactModel("Einkaufen", 1));
-		// contactRepository.save(new ContactModel("Seminararbeit erstellen", 2));
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ContactApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        try {
+            contactRepository.deleteAll();
+            addressRepository.deleteAll();
+
+            Address address1 = new Address("Straße 1", "",
+                    "80963", "München", Country.GERMANY);
+            Contact contact1 = new Contact("Thorsten", "Schlott");
+            Contact contact2 = new Contact("Max", "Mustermann", Gender.MALE,
+                    LocalDate.of(1999, Month.JULY, 23), "test@test.de",
+                    "+49012940323", address1);
+
+            addressRepository.save(address1);
+            contactRepository.save(contact1);
+            contactRepository.save(contact2);
+        } catch (Exception exception) {
+            logger.error(exception.getMessage());
+        }
+    }
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
 
 }
