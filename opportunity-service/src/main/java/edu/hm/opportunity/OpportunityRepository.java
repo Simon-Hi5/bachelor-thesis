2dc1263b0c10297d5128f6042fbede4bf2176658
SimonHirner <simon.hirner@hm.edu>
2021-12-27 17:52:09 +0100

    Refactor code.

--- opportunity-service/src/main/java/edu/hm/opportunity/OpportunityRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.hm.opportunity;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OpportunityRepository extends MongoRepository<OpportunityModel, String> {
-
-}
