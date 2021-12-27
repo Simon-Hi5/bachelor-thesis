003e7db3e81266706900d8529081e005695944f9
SimonHirner <simon.hirner@hm.edu>
2021-12-27 16:48:48 +0100

    Add smoke tests.

--- contact-microservice/src/test/java/edu/hm/contact/ContactApplicationTests.java
+++ contact-microservice/src/test/java/edu/hm/contact/ContactApplicationTests.java
@@ -1,13 +1,21 @@
 package edu.hm.contact;
 
+import edu.hm.contact.controller.ContactController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ContactApplicationTests {
 
+	@Autowired
+	private ContactController contactController;
+
 	@Test
 	void contextLoads() {
+		assertThat(contactController).isNotNull();
 	}
 
 }
