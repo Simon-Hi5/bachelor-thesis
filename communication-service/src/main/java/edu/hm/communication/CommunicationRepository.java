f96fcd311d3ff8eda5e78bd408cff288b0b15d17
SimonHirner <simon.hirner@hm.edu>
2021-12-27 17:52:09 +0100

    Refactor code.

--- communication-service/src/main/java/edu/hm/communication/CommunicationRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.hm.communication;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CommunicationRepository extends MongoRepository<CommunicationModel, String> {
-
-}
