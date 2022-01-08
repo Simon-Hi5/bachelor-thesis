f116ac73259049382bd269a86fff43a19e266183
Simon Hirner <simon.hirner99@gmail.com>
2022-01-08 23:52:37 +0100

    Remove template for microservices.

--- template-service/src/main/java/edu/hm/template/TemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.hm.template;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TemplateRepository extends MongoRepository<TemplateModel, String> {
-
-}
