df867cc57c467503604e53c8cb3b71b31ac9ddf5
Simon Hirner <simon.hirner99@gmail.com>
2022-01-09 15:38:30 +0100

    Add routing for frontend.

---
+++ crm-frontend/src/pages/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactList from '../components/contact/ContactList';
+
+const Contacts = () => {
+    return (
+        <div>
+            <h2>Contacts</h2>
+            <ContactList />
+        </div>
+    );
+}
+
+export default Contacts;
