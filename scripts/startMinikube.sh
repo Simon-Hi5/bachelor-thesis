d5833e1109cd32d70320de867ac3a07f39f1ebe6
Simon Hirner <simon.hirner99@gmail.com>
2022-01-12 00:05:47 +0100

    Add scripts for kubernetes deployment.

--- scripts/startMinikube.sh
+++ scripts/startMinikube.sh
@@ -4,4 +4,16 @@
 sh ./stopMinikube.sh
 
 # Start new container
-minikube start --driver=virtualbox --memory 5000 --cpus 2
+minikube start --driver=virtualbox --memory 5000 --cpus 2 --insecure-registry "192.168.59.117:30400"
+
+# Deploy private registry
+./deployRegistry.sh
+
+# Add insecure registry
+./add-insecure-registry.sh
+
+# Build images from dockerfiles
+docker build -f ../contact-microservice/Dockerfile -t $(minikube ip):30400/contact-microservice:latest ../contact-microservice
+
+# Push images to private registry
+docker push $(minikube ip):30400/contact-microservice:latest
