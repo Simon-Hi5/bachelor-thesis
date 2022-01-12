e62010ce78de9879d778e5d2bad193cc8ad6f14c
Simon Hirner <simon.hirner99@gmail.com>
2022-01-12 22:40:31 +0100

    Refactor code.

--- contact-microservice/src/test/java/edu/hm/contact/controller/ContactControllerTest.java
+++ contact-microservice/src/test/java/edu/hm/contact/controller/ContactControllerTest.java
@@ -2,12 +2,9 @@ package edu.hm.contact.controller;
 
 import edu.hm.contact.service.ContactService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
