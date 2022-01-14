a192c41bf8fc307eb0bb23474c1e026bd92ecd89
Simon Hirner <simon.hirner99@gmail.com>
2022-01-14 23:28:34 +0100

    Change kubernetes deployment.

--- scripts/kube/deploy-opportunity-microservice.sh
+++ scripts/kube/deploy-opportunity-microservice.sh
@@ -5,7 +5,7 @@ eval "$(minikube docker-env)"
 
 # Deploy MongoDB
 kubectl apply -f ../../kubernetes/opportunity-mongodb.yaml
-kubectl rollout status deployments/opportunity-db
+kubectl rollout status statefulsets/opportunity-db
 
 # Build image from dockerfile
 docker build -f ../../opportunity-microservice/Dockerfile -t opportunity-microservice:latest ../../opportunity-microservice
